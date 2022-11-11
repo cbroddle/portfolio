ce6391d37fc7ce59edebafafaff3d116d5667cd2
cbroddle <christenbroddle@gmail.com>
2022-11-10 22:23:25 -0800

    Fix form attribute, test again

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//mobile nav menu
-const mobileNav = document.querySelector(".mobile-nav");
 
 // menu icon toggle
 const menuToggle = document.querySelector(".menu-icon");
 // menu
 const menuSidebar = document.querySelector(".menu-container");
+
 
 // menu event listener
 menuToggle.addEventListener("click", function () {
